7fa083be6f0ffd931860bdc7564f0728843ad000
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-07-21 15:05:10 +0200

    pre-commit fixes

--- profiles/nersc-batch/nersc-submit.sh
+++ profiles/nersc-batch/nersc-submit.sh
@@ -20,7 +20,7 @@ for s in simids:
 
 for s in $simids; do
     echo ">>> $s"
-    snakemake --config simlist=$s --dry-run | grep 'Nothing to be done' && continue
+    snakemake --config simlist="$s" --dry-run | grep 'Nothing to be done' && continue
 
     echo "Submitting..."
     sbatch \
