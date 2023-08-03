0a0f3a6ad87f22dd16e3865a73ac878435aae320
William Quinn <wquinn@legend-login2.lngs.infn.it>
2023-08-04 01:26:44 +0200

    Big change. tree.Project had limitations on applying cuts so moved to looping over the entries in the tree and filling the TH1Fs stored in a dict. Added a config file.

--- scripts/build_pdf.py
+++ scripts/build_pdf.py
@@ -21,6 +21,7 @@ import argparse
 
 import ROOT
 import uproot
+import json
 from legendmeta import LegendMetadata
 
 parser = argparse.ArgumentParser(
@@ -38,8 +39,9 @@ chmap = meta.channelmap("20230323T000000Z")
 if not isinstance(args.input_files, list):
     args.input_files = [args.input_files]
 
+rconfig = json.load(open("build_pdf_config.json"))
 
-def process_mage_id(mage_id, chmap):
+def process_mage_id(mage_id):
     m_id = str(mage_id)
     is_ged = bool(int(m_id[0]))
     if not is_ged:
@@ -51,23 +53,14 @@ def process_mage_id(mage_id, chmap):
     for key, value in chmap.items():
         if isinstance(value, dict) and "location" in value:
             location = value["location"]
-            if location.get("string") == string and location.get("position") == pos:
+            usable = value['analysis']["usability"]=='on' 
+            if location.get("string") == string and location.get("position") == pos and usable:
                 return {"name": key, "ch": value["daq"]["rawid"], "mage_id": mage_id}
 
     return False
 
-
-# TODO: Do we need a config file?
-# list of cuts - this could / should be moved to a config file
-# Could also include the bins and widths
-cuts = {
-    "raw": "",
-    "mul": "mage_id@.size()==1",
-    "lar": "npe_tot==0",
-    "mul_lar": "mage_id@.size()==1 && npe_tot==0",
-}
-
 for file_name in args.input_files:
+
     # Get the tree from the file
     file = ROOT.TFile(file_name, "READ")
     tree = file.Get("simTree")
@@ -76,51 +69,66 @@ for file_name in args.input_files:
         {mage_id for n in [list(tree.mage_id) for entry in tree] for mage_id in n}
     )
     mage_names = {
-        mage_id: process_mage_id(mage_id, chmap)
+        mage_id: process_mage_id(mage_id)
         for mage_id in uniq_mage_ids
-        if process_mage_id(mage_id, chmap)
+        if process_mage_id(mage_id)
     }
 
+    # Define a dictionary to contain all the histograms to be filled
+    # for each cut and each detector
+    hists = {
+        f"{cut_name}": {
+            _mage_id: ROOT.TH1F(
+                    f"{cut_name}_{_mage_names['ch']}", f"{_mage_names['name']} energy deposition",
+                    rconfig['hist']['nbins'], rconfig['hist']['emin'], rconfig['hist']['emax']
+                ) for _mage_id, _mage_names in mage_names.items()
+        } for cut_name in rconfig["cuts"].keys()
+    }
+    # Add the grouped dets hists
+    for cut_name in rconfig["cuts"].keys():
+        for _type in ["bege", "coax", "icpc", "ppc"]:
+            hists[cut_name][f"{_type}"] = ROOT.TH1F( 
+                f"{cut_name}_type_{_type}", f"All {_type} energy deposit",
+                    rconfig['hist']['nbins'], rconfig['hist']['emin'], rconfig['hist']['emax']
+            ) 
+        hists[cut_name]['all'] = ROOT.TH1F( 
+            f"{cut_name}_type_all", f"All energy deposit",
+            rconfig['hist']['nbins'], rconfig['hist']['emin'], rconfig['hist']['emax']
+        ) 
+
     out_file_name = file_name.split("/")[-1].split(".")[0] + "_pdf" + ".root"
-    out_file = uproot.create(args.output + out_file_name)
+    out_file = uproot.recreate(args.output + out_file_name)
 
-    for cut_name, cut_string in cuts.items():
+    # Loop over the data ONCE and fill as we go along
+    for entry in tree:
+        # In the real data we have an energy threshold defined in the config
+        energy_list  = [val for val in entry.energy if val >= rconfig['energy_threshold']]
+        mage_id_list = [val for index, val in enumerate(entry.mage_id) if entry.energy[index] >= rconfig['energy_threshold']]
+        npe_tot = entry.npe_tot
+        if len(energy_list) == 0: continue  # Nothing to fill
+
+        for cut_name, cut_string in rconfig["cuts"].items():
+
+            # In the config is a lambda function string that returns true or false for each cut
+            # Maybe this is too much but it allows for more cuts possibly
+            exec(cut_string)
+            pass_cut = func(energy_list, mage_id_list, npe_tot)
+            if not bool(pass_cut): continue
+
+            for mage_id, energy in zip(mage_id_list, energy_list):
+                energy_kev = energy * 1000
+                mage_dict = process_mage_id(mage_id)
+
+                if mage_dict == False: continue # not an 'on' detector so we don't fill
+                hists[cut_name][mage_id].Fill(energy_kev)
+                hists[cut_name][chmap[mage_dict["name"]]["type"]].Fill(energy_kev)
+                hists[cut_name]['all'].Fill(energy_kev)
+
+    # Create a new root file with the directories
+    for cut_name in rconfig["cuts"].keys():
         dir = out_file.mkdir(cut_name)
-
-        # Define the grouped histos to be updated
-        geds_dict = {
-            f"{i}": ROOT.TH1D(f"type_{i}", f"All {i} energy deposit", 3000, 0, 3000)
-            for i in ["bege", "coax", "icpc", "ppc"]
-        }
-
-        # loop over the channels
-        for mage_id, mg_dict in mage_names.items():
-            if cut_string == "":
-                string = f"mage_id=={mage_id}"
-            else:
-                string = f"mage_id=={mage_id} && {cut_string}"
-
-            hist = ROOT.TH1D(
-                f'ch{mg_dict["ch"]}', f'{mg_dict["name"]} energy deposit', 3000, 0, 3000
-            )  # units of keV and bin width of 1keV
-            tree.Project(hist.GetName(), "energy * 1000", string)  # add cut
-            dir[hist.GetName()] = hist
-
-            geds_dict[chmap[mg_dict["name"]]["type"]].Add(hist)
-
-            del hist
-
-        for _ged_type, hist in geds_dict.items():
-            dir[hist.GetName()] = hist
-        del geds_dict
-
-        # Then finally ALL
-        hist = ROOT.TH1D(
-            "all", "All channels energy deposit", 3000, 0, 3000
-        )  # units of keV and bin width of 1keV
-        tree.Project(hist.GetName(), "energy * 1000", cut_string)  # add cut
-        dir[hist.GetName()] = hist
-        del hist
+        for key, item in hists[cut_name].items():
+            dir[item.GetName()] = item
 
     out_file.close()
     file.Close()
