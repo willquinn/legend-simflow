4a2b8b3d7dc8890e353546dc7b9a82d7e438c82a
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-08-17 12:57:56 +0200

    Refactor nerc-submit.sh

--- profiles/nersc-batch/nersc-submit.sh
+++ profiles/nersc-batch/nersc-submit.sh
@@ -1,11 +1,59 @@
 #!/bin/bash
 
-# module load python
-# mamba activate snakemake
+function make_version() {
+    version="$(basename "$PWD")"
+    echo "$(basename "$(dirname "$PWD")")-$version"
+}
 
-version=$(basename "$PWD")
+function sbatch_submit() {
+    local job="$1"
+    local logdir="$2"
+    shift 2
 
-# rm -rf .snakemake
+    echo "INFO: submitting $job..."
+    sbatch \
+        --nodes 1 \
+        --ntasks-per-node=1 \
+        --account m2676 \
+        --constraint cpu \
+        --time 12:00:00 \
+        --qos regular \
+        --licenses scratch,cfs \
+        --job-name "$job" \
+        --output "$logdir/$job.log" \
+        --error "$logdir/$job.log" \
+        --wrap "
+            srun snakemake \
+                --shadow-prefix $PSCRATCH \
+                $*
+        "
+}
+
+function sbatch_submit_simid() {
+    local simid="$1"
+    local job
+    job="$(make_version)::$simid"
+
+    echo "INFO: inspecting $job"
+    is_job_in_queue "$job" && return
+
+    snakemake --config simlist="$simid" --dry-run 2> /dev/null | grep 'Nothing to be done' && return
+
+    sbatch_submit "$job" "$logdir" --config simlist="$simid"
+}
+
+function is_job_in_queue() {
+    if squeue --me --format '%200j' | grep "$1"; then
+        echo "INFO: job already queued"
+        return 0
+    else return 1
+    fi
+}
+
+export -f make_version
+export -f sbatch_submit
+export -f sbatch_submit_simid
+export -f is_job_in_queue
 
 logdir=".slurm/$(date +'%Y%m%dT%H%M%SZ')"
 mkdir -p "$logdir"
@@ -21,64 +69,18 @@ for s in simids:
     print(f"pdf.{s}", end=" ")
     ')
 
-    for s in $simids; do
-        job="${version}_$s"
-        echo "INFO: inspecting $job"
+    # shellcheck disable=SC2086
+    parallel --citation --jobs 10 sbatch_submit_simid ::: $simids
 
-        if squeue --me --format '%200j' | grep "$job"; then
-            echo "INFO: job already queued, skipping"
-            continue
-        fi
-
-        snakemake --config simlist="$s" --dry-run | grep 'Nothing to be done' && continue
-
-        echo "INFO: submitting..."
-        # https://docs.nersc.gov/development/shifter/faq-troubleshooting/#failed-to-lookup-image
-        sbatch \
-            --nodes 1 \
-            --ntasks-per-node=1 \
-            --account m2676 \
-            --constraint cpu \
-            --time 12:00:00 \
-            --qos regular \
-            --licenses scratch,cfs \
-            --job-name "$job" \
-            --output "$logdir/$s.log" \
-            --error "$logdir/$s.log" \
-            --image "legendexp/legend-base:latest" \
-            --wrap "
-                srun snakemake \
-                    --shadow-prefix $PSCRATCH \
-                    --config simlist=$s
-            "
-    done
 else
-    job="${version}-legend-pdfs"
+    job="$(make_version)::all"
 
-    if squeue --me --format '%200j' | grep "$job"; then
-        echo "INFO: job already queued"
-        exit 1
-    fi
+    echo "INFO: inspecting $job"
+    is_job_in_queue "$job" && exit 1
 
-    snakemake --dry-run "$@" | grep 'Nothing to be done' && exit 1
+    snakemake --dry-run "$@" 2> /dev/null | grep 'Nothing to be done' && exit 1
 
-    echo "INFO: submitting..."
-    # https://docs.nersc.gov/development/shifter/faq-troubleshooting/#failed-to-lookup-image
-    sbatch \
-        --nodes 1 \
-        --ntasks-per-node=1 \
-        --account m2676 \
-        --constraint cpu \
-        --time 12:00:00 \
-        --qos regular \
-        --licenses scratch,cfs \
-        --job-name "$job" \
-        --output "$logdir/$job.log" \
-        --error "$logdir/$job.log" \
-        --image "legendexp/legend-base:latest" \
-        --wrap "
-            srun snakemake \
-                --shadow-prefix $PSCRATCH \
-                $*
-        "
+    sbatch_submit "$job" "$logdir" "$*"
 fi
+
+echo "INFO: logs in $(realpath "$logdir")"
