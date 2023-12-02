e287615bf09653b8aa17c65b657a84d9cc4f61cf
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-12-02 19:02:27 +0100

    pre-commit fixes

--- scripts/k42/run_split_K42_vertices.sh
+++ scripts/k42/run_split_K42_vertices.sh
@@ -4,12 +4,12 @@ doit() {
     echo "INFO: spawning job $1"
     shifter --image=legendexp/legend-software:latest -- \
         ./workflow/scripts/split_K42_vertices.C \
-	generated/tier/raw/l200a-lar-cylinder-K42/l200a-lar-cylinder-K42_$1-tier_raw.root \
-        generated/tier/raw/l200a-lar-inside-ms-K42/l200a-lar-inside-ms-K42_$1-tier_raw.root \
-	generated/tier/raw/l200a-lar-outside-ms-K42/l200a-lar-outside-ms-K42_$1-tier_raw.root \
-	&> /dev/null
+        "generated/tier/raw/l200a-lar-cylinder-K42/l200a-lar-cylinder-K42_$1-tier_raw.root" \
+        "generated/tier/raw/l200a-lar-inside-ms-K42/l200a-lar-inside-ms-K42_$1-tier_raw.root" \
+        "generated/tier/raw/l200a-lar-outside-ms-K42/l200a-lar-outside-ms-K42_$1-tier_raw.root" \
+        &> /dev/null
 }
 export -f doit
 
-nfiles=$(ls generated/tier/raw/l200a-lar-cylinder-K42/ | wc -l)
-seq -f "%04g" 0 $(($nfiles-1)) | parallel doit {}
+nfiles=$(find generated/tier/raw/l200a-lar-cylinder-K42 -maxdepth 1 -mindepth 1 | wc -l)
+seq -f "%04g" 0 $((nfiles-1)) | parallel doit {}
