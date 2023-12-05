6d38a28f37f53a7ae5216f6ee8525d3b865ebd76
LEGEND lgsim user at NERSC <analysis@legend-exp.org>
2023-12-05 03:41:18 -0800

    Fix K42 run stript again

--- scripts/k42/run_split_K42_vertices.sh
+++ scripts/k42/run_split_K42_vertices.sh
@@ -6,7 +6,7 @@ doit() {
         return
     fi
     echo "INFO: spawning job $1"
-    shifter --image=legendexp/legend-software:latest -- \
+    shifter --image=legendexp/legend-software:latest -- root -l -q -x \
         "./workflow/scripts/k42/split_K42_vertices.C(
         \"generated/tier/raw/l200a-lar-cylinder-K42/l200a-lar-cylinder-K42_$1-tier_raw.root\",
         \"generated/tier/raw/l200a-lar-inside-ms-K42/l200a-lar-inside-ms-K42_$1-tier_raw.root\",
