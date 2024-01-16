a904ada46777bdeb91b010c05e4747e97241ab13
William Stanley Quinn <wquinn@login06.chn.perlmutter.nersc.gov>
2024-01-16 02:24:25 -0800

    Added the code for dealing with categories for 2d hists. I also removed some redundant code and added some comments

--- scripts/build_pdf.py
+++ scripts/build_pdf.py
@@ -29,7 +29,14 @@ from legendmeta import LegendMetadata
 
 
 def process_mage_id(mage_ids):
-    mage_names = {}
+    """
+    """
+    mage_names = {
+        "name": {},
+        "channel": {},
+        "position": {},
+        "string": {}
+    }
     for _mage_id in mage_ids:
         m_id = str(_mage_id)
         is_ged = bool(int(m_id[0]))
@@ -44,9 +51,62 @@ def process_mage_id(mage_ids):
             if _meta_dict["system"] == "geds":
                 location = _meta_dict["location"]
                 if location["string"] == string and location["position"] == pos:
-                    mage_names[_mage_id] = f"ch{_meta_dict['daq']['rawid']}"
+                    mage_names["channel"][_mage_id] = f"ch{_meta_dict['daq']['rawid']}"
+                    mage_names["name"][_mage_id] = _name
+                    mage_names["string"][_mage_id] = int(string)
+                    mage_names["position"][_mage_id] = int(pos)
 
     return mage_names
+
+
+def get_m2_categories(channel_array, channel_to_string, channel_to_position):
+    """
+    Get the categories for the m2 data based on 3 categories defined in the config
+    Categories:
+        1 - Same string vertical neighbour
+        2 - Same string not vertical neighbor
+        3 - Different string
+    Parameters:
+        channel_array: 2D numpy array of channels
+        channel2string: vectorised numpy function to convert channel into string
+        channel2position: vectorised numpy function to convert channel into position
+    Returns:
+        categories: list of categories per event
+    """
+    
+    channel_array = np.vstack(channel_array)
+    channel_one = channel_array[:,0].T
+    channel_two = channel_array[:,1].T
+    
+    ## convert to the list of strings
+    string_one = channel_to_string(channel_one)
+    string_two = channel_to_string(channel_two)
+
+    same_string = string_one == string_two
+    position_one = channel_to_position(channel_one)
+    position_two = channel_to_position(channel_two)
+    neighbour = np.abs(position_one - position_two) == 1
+    
+    is_cat_one = (same_string) & (neighbour)
+    is_cat_two = (same_string) & (~neighbour)
+    is_cat_three = (~same_string)
+    category = 1*is_cat_one+2*is_cat_two+3*is_cat_three
+    return np.array(category)
+
+
+def get_vectorised_converter(mapping):
+    """ Create a vectorized function converting channel to some other quantity based on a dict
+        Parameters:
+             - mapping: a python dictonary of the mapping 
+        Return:
+             - a numpy vectorised function of this mapping
+    """
+    def channel_to_other(mage_id):
+        """ Extract which string a given channel is in"""
+
+        return mapping[mage_id]
+
+    return np.vectorize(channel_to_other)
 
 
 parser = argparse.ArgumentParser(
@@ -74,18 +134,27 @@ with Path(args.config).open() as f:
 
 meta = LegendMetadata(args.metadata)
 chmap = meta.channelmap(rconfig["timestamp"])
+# We don't have a list of mage_ids so these need to be instantiated and checked
+# in the loop of sim data
+chmap_mage = None
+channel_to_string = None
+channel_to_position = None
+
 geds_mapping = {
     f"ch{_dict['daq']['rawid']}": _name
     for _name, _dict in chmap.items()
     if chmap[_name]["system"] == "geds"
 }
+
 n_primaries_total = 0
 
+# NOTE: This doesn't seem to work, returns zeros
 print("INFO: computing number of simulated primaries from raw files")
 if args.raw_files:
     for file in args.raw_files:
         with uproot.open(f"{file}:fTree") as fTree:
             n_primaries_total += fTree["fNEvents"].array(entry_stop=1)[0]
+print("INFO: nprimaries", n_primaries_total)
 
 # So there are many input files fed into one pdf file
 # set up the hists to fill as we go along
@@ -118,7 +187,6 @@ sum_hists = {
     )
     for _cut_name in rconfig["cuts"]
     if rconfig["cuts"][_cut_name]["is_sum"] is True
-    and rconfig["cuts"][_cut_name]["is_2d"] is False
 }
 
 # We want some 2d hists as well
@@ -139,6 +207,7 @@ hists_2d = {
 
 for file_name in args.input_files:
     print("INFO: loading file", file_name)
+
     with uproot.open(f"{file_name}:simTree") as pytree:
         if pytree.num_entries == 0:
             msg = f"ERROR: MPP evt file {file_name} has 0 events in simTree"
@@ -156,26 +225,34 @@ for file_name in args.input_files:
     rng = np.random.default_rng()
     df_data["npe_tot_poisson"] = rng.poisson(df_data.npe_tot)
 
+    # Data has awkward length lists per event
+    # exploding gives a dataframe with multiple rows per event (event no. is the index)
     df_exploded = df_data.explode(["energy", "mage_id", "is_good"])
+    
+    # Doing this over and over again maybe slow
+    chmap_mage = process_mage_id(df_exploded.dropna(subset=["mage_id"])["mage_id"].unique())
+    channel_to_string = get_vectorised_converter(chmap_mage["string"])
+    channel_to_position = get_vectorised_converter(chmap_mage["position"])
+
+    # Apply the real energy cut for effetcive event reconstruction
     df_ecut = df_exploded.query(f"energy > {rconfig['energy_threshold']}")
 
     # These give you the multiplicity of events (and events not including AC detectors)
     index_counts = df_ecut.index.value_counts()
     index_counts_is_good = df_ecut.query("is_good == True").index.value_counts()
 
+    # Add columns for configuration file cuts (NOTE: quite slow)
     df_ecut = df_ecut.copy()
     df_ecut["mul"] = df_ecut.index.map(index_counts)
     df_ecut["mul_is_good"] = df_ecut.index.map(index_counts_is_good)
 
-    if not args.raw_files:
-        n_primaries_total += n_primaries
+    n_primaries_total += n_primaries
 
-    uniq_mage_ids = df_exploded.dropna(subset=["mage_id"])["mage_id"].unique()
-    mage_names = process_mage_id(uniq_mage_ids)
-    # out_file["number_of_primaries"] = str(n_primaries)
+    # uniq_mage_ids = df_exploded.dropna(subset=["mage_id"])["mage_id"].unique()
+    # mage_names = process_mage_id(uniq_mage_ids)
 
     for _cut_name, _cut_dict in rconfig["cuts"].items():
-        # We want to cut on multiplicity for all detectors >25keV
+        # We want to cut on multiplicity for all detectors >25keV, even AC
         # Include them in the dataset then apply cuts - then filter them out
         # Don't store AC detectors
         _cut_string = _cut_dict["cut_string"]
@@ -185,9 +262,9 @@ for file_name in args.input_files:
 
         if _cut_dict["is_sum"] is False and _cut_dict["is_2d"] is False:
             for __mage_id in df_good.mage_id.unique():
-                _rawid = mage_names[__mage_id]
+                _rawid = chmap_mage["channel"][__mage_id]
                 _energy_array = (
-                    df_good.query(f"mage_id == {__mage_id}").energy.to_numpy(
+                    df_good[df_good.mage_id == __mage_id].energy.to_numpy(
                         dtype=float
                     )
                     * 1000
@@ -196,26 +273,38 @@ for file_name in args.input_files:
                 hists[_cut_name][_rawid].FillN(
                     len(_energy_array), _energy_array, np.ones(len(_energy_array))
                 )
-        elif _cut_dict["is_sum"] is False and _cut_dict["is_2d"] is True:
+        elif _cut_dict["is_2d"] is True:
+            category = _cut_dict["category_desc"]["category"]
             _energy_1_array = (
                 df_good.groupby(df_good.index).energy.max().to_numpy(dtype=float)
             ) * 1000
             _energy_2_array = (
                 df_good.groupby(df_good.index).energy.min().to_numpy(dtype=float)
             ) * 1000
+
+            if category > 0:
+                # NOTE: This is weird 
+                _mult_channel_array = (df_good.groupby(df_good.index).mage_id.apply(lambda x: x.to_numpy()).to_numpy())
+                
+                categories = get_m2_categories(_mult_channel_array, channel_to_string, channel_to_position)
+
+                _energy_1_array=np.array(_energy_1_array)[np.where(categories==category)[0]]
+                _energy_2_array=np.array(_energy_2_array)[np.where(categories==category)[0]]
+
             hists_2d[_cut_name].FillN(
                 len(_energy_1_array),
                 _energy_2_array,
                 _energy_1_array,
                 np.ones(len(_energy_1_array)),
             )
+                
         else:
             _summed_energy_array = (
                 df_good.groupby(df_good.index).energy.sum().to_numpy(dtype=float) * 1000
             )  # keV
             if len(_summed_energy_array) == 0:
                 continue
-
+            
             sum_hists[_cut_name].FillN(
                 len(_summed_energy_array),
                 _summed_energy_array,
@@ -224,7 +313,7 @@ for file_name in args.input_files:
 
 # The individual channels have been filled
 # now add them together to make the grouped hists
-# We don't need to worry about the ac dets as they will have zero entries
+# We don't need to worry about the AC dets as they will have zero entries
 print("INFO: making grouped pdfs")
 for _cut_name in hists:
     hists[_cut_name]["all"] = ROOT.TH1F(
@@ -258,15 +347,12 @@ for _cut_name, _hist_dict in hists.items():
         if item.GetEntries() > 0:
             dir[key] = item
 
-for _cut_name, _hist in sum_hists.items():
-    dir = out_file.mkdir(_cut_name)
-    if _hist.GetEntries() > 0:
-        dir["all"] = _hist
-
-for _cut_name, _hist in hists_2d.items():
-    dir = out_file.mkdir(_cut_name)
-    if _hist.GetEntries() > 0:
-        dir["all"] = _hist
-
+# All other hists
+for dict in [sum_hists, hists_2d]:
+    for _cut_name, _hist in dict.items():
+        dir = out_file.mkdir(_cut_name)
+        if _hist.GetEntries() > 0:
+            dir["all"] = _hist
+print("INFO: nprimaries", n_primaries_total)
 out_file["number_of_primaries"] = str(int(n_primaries_total))
 out_file.close()
